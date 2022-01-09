85c0a36787df1728699f2490eb8aa803c728fe66
Varun Naik <naik.varun@gmail.com>
2022-01-09 17:32:18 -0600

    Created basic app

---
+++ src/common/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          MBI Generater
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
